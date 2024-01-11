505d5886579c5ba52fe481b337d23165bfe752f3
StefanoColamonaco <stef_2000@hotmail.it>
2024-01-11 20:31:11 +0100

    First try
    Co-authored-by: Antonio Lopez <elements72@users.noreply.github.com>
    Co-authored-by: AndreaZecca <AndreaZecca@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,31 +1,81 @@
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
-from langchain.retrievers import PubMedRetriever
+from langchain_community.retrievers import PubMedRetriever
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-import dotenv
-from pathlib import Paths
+from dotenv import load_dotenv
 
+import langchain
+from langchain.prompts.pipeline import PipelinePromptTemplate
+from langchain.prompts.prompt import PromptTemplate
+
+from pathlib import Path
+
+
+langchain.verbose = True
 
 templates_path = Path('./templates')
 
-def create_chain(model_id, model_kwargs, opt4=True):
-    if opt4:
-        with open(templates_path / 'medqa_opt4.txt') as f:
-            template = f.readlines()
-    else:
-        with open(templates_path / 'medqa_opt5.txt') as f:
-            template = f.readlines()
+templates = {
+    "zephyr": {
+        "sys_start": '<|system|>\n',
+        "sys_end": '</s>\n',
+        "usr_start": '<|user|>\n',
+        "usr_end": '</s>\n',
+        "assistant_start": '<|assistant|>\n'
+    }
+}
 
-    prompt = PromptTemplate.from_template(template)
+
+def create_prompt(model_name):
+    full_template = """
+    {system}
+
+    {usr_msg}
+    """
+    model_template = templates[model_name]
+    full_prompt = PromptTemplate.from_template(full_template)
+    introduction_template = f"{model_template['sys_start']}You are a medical expert. Just answer the question as concise as possible based on the user needs. Always return the response in the expected format required by the user. {model_template['sys_end']}"
+    introduction_prompt = PromptTemplate.from_template(introduction_template)
+
+    with open(templates_path / 'medqa_opt4.txt') as f:
+        usr_template = f.read()
+
+    usr_template = usr_template.format(
+        usr_start=model_template['usr_start'],
+        usr_end=model_template['usr_end'],
+        assistant_start=model_template['assistant_start']
+    )
+    usr_prompt = PromptTemplate.from_template(usr_template)
+
+
+    input_prompts = [
+    ("system", introduction_prompt),
+    ("usr_msg", usr_prompt)]
+
+    pipeline_prompt = PipelinePromptTemplate(
+    final_prompt=full_prompt, pipeline_prompts=input_prompts)
+
+    #print(pipeline_prompt.input_variables)
+    return pipeline_prompt
+
+
+
+    
+
+def create_chain(model_id, model_kwargs, opt4=True):
+
+    prompt = create_prompt("zephyr")
+   # print('Prompt')
 
     retriever = PubMedRetriever()
 
     llm = HuggingFacePipeline.from_model_id(
         model_id=model_id,
-        device='auto',
+        device=0,
         task="text-generation",
-        pipeline_kwargs=model_kwargs,
+        model_kwargs=model_kwargs,
+        pipeline_kwargs={"max_new_tokens": 100},
     )
 
     def format_docs(docs):
@@ -38,8 +88,17 @@ def create_chain(model_id, model_kwargs, opt4=True):
     | llm
     | StrOutputParser()
     )
+    response = rag_chain.invoke("""A junior orthopaedic surgery resident is completing a carpal tunnel repair with the department chairman as the attending physician. During the case, the resident inadvertently cuts a flexor tendon. The tendon is repaired without complication. The attending tells the resident that the patient will do fine, and there is no need to report this minor complication that will not harm the patient, as he does not want to make the patient worry unnecessarily. He tells the resident to leave this complication out of the operative report. Which of the following is the correct next action for the resident to take?
+    (A) Disclose the error to the patient and put it in the operative report
+    (B) Tell the attending that he cannot fail to disclose this mistake
+    (C) Report the physician to the ethics committee
+    (D) Refuse to dictate the operative report
+    """
+    )
+    print(response)
         
 
 
-if __name__ == "main":
-    dotenv.load('./env')
+
+create_chain("HuggingFaceH4/zephyr-7b-beta", {"torch_dtype": "auto"})
+
