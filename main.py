a90c219703ead7a50aa2e11694ee5c73025769e8
StefanoColamonaco <stef_2000@hotmail.it>
2024-01-12 19:39:58 +0100

    Cleaned old files and rename
    Co-authored-by: MatteoVannucchi0 <MatteoVannucchi0@users.noreply.github.com>
    Co-authored-by: AndreaZecca <AndreaZecca@users.noreply.github.com>
    Co-authored-by: Antonio Lopez <elements72@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables import RunnablePassthrough
+from langchain_community.vectorstores import Milvus
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
-import spacy
+
 from dotenv import load_dotenv
 from pathlib import Path
-from utils import get_template, load_entity_linker
+from utils import get_template
 from parse_dataset import parse_dataset, get_index_from_res
 
 load_dotenv()
@@ -19,25 +21,7 @@ templates_path = Path('./templates')
 usr_template = None
 
 models = [ "HuggingFaceH4/zephyr-7b-beta", "meta-llama/Llama-2-7b-chat-hf", "mistralai/Mistral-7B-Instruct-v0.1" ]
-datasets = [ "medqa4", "medqa5", "altro_dataset" ]
-
-model_id = models[2]
-dataset = datasets[0]
-
-with open(get_template(model_id, dataset)) as f:
-        usr_template = f.read()
-
-prompt = ChatPromptTemplate.from_template(usr_template)
-
-llm = HuggingFacePipeline.from_model_id(
-        model_id=model_id,
-        device=0,
-        task="text-generation",
-        model_kwargs={"torch_dtype": "auto", "temperature": 1e-10, "do_sample": True},
-        pipeline_kwargs={"max_new_tokens": 100},
-    )
-
-# retriever = PubMedRetriever(search_type="similarity_score_threshold", top_k_results=5, score_thresholds=0.1, search_kwargs={"score_threshold": 0.01})
+datasets = [ "medqa_opt4", "medqa_opt5", "medncqa_opt4" ][:2]
 
 
 
@@ -46,46 +30,63 @@ def format_docs(docs):
         f.write(str(docs) + '\n')
     return "\n\n".join([d.page_content for d in docs])
 
-nlp = load_entity_linker()
-
-def print_query(query):
-    print("QUERY:", query)
-    return query
-
-def format_query(query):
-    # take only from (A)
-    query = query.split("(A)")[1]
-
-    doc = nlp(query)
-    print("DOC:",doc)
-    with open('log_query.txt', "w") as f:
-        full_text = ""
-        full_labels = ""
-        for ent in doc.ents:
-            full_labels += ent.label_ + " "
-            full_text += ent.text + " "
-        full_text += "\n"
-        full_labels += "\n"
-        f.write(full_text)
-        f.write(full_labels)
-    # Qui andrebbe la formattazione / ricerca delle keywords
-    return full_text
-
-
-chain = (
-    {"context": format_query | retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
+model_id = models[2]
+llm = HuggingFacePipeline.from_model_id(
+        model_id=model_id,
+        device=0,
+        task="text-generation",
+        model_kwargs={"torch_dtype": "auto", "temperature": 1e-10, "do_sample": True},
+        pipeline_kwargs={"max_new_tokens": 100},
 )
 
-questions = parse_dataset("medqa_4opt.jsonl")
+def test_dataset(dataset):
+    collection_name = 'medqa' if dataset in ['medqa_opt4', 'medqa_opt5'] else 'medncqa_opt4'
 
-for q in questions[:1]:
-    res = chain.invoke(q["question"])
-    print(res)
-    index = get_index_from_res(res)
-    if index == q["answer_idx"]:
-        print("Correct!")
-    else:
-        print("Wrong!")
+    with open(get_template(model_id, dataset)) as f:
+            usr_template = f.read()
+
+    prompt = ChatPromptTemplate.from_template(usr_template)
+
+
+
+    embedder = HuggingFaceEmbeddings(model_name="neuml/pubmedbert-base-embeddings")
+
+    vector_db = Milvus(
+        embedder,
+        connection_args={"host": "127.0.0.1", "port": "19530"},
+        collection_name=collection_name,
+    )
+    # Retrieve and generate using the relevant snippets of the blog.
+    retriever = vector_db.as_retriever()
+
+
+    chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    questions = parse_dataset(f"{dataset}.jsonl")
+
+    correct = 0
+
+    for q in questions[:10]:
+        res = chain.invoke(q["question"])
+        print(f"Response : {res} \n\n")
+        index = get_index_from_res(res)
+        if index == q["answer_idx"]:
+            correct += 1
+            print("The answer is correct")
+        else:
+            model_answer = index if index != "F" else 'No answer'
+            if model_answer == "No answer":
+                print(res)
+    
+            print(f"The answer is wrong, model answer: {model_answer}, correct {q['answer_idx']}")
+
+
+for dataset in datasets:
+    print(f"Testing dataset {dataset}")
+    test_dataset(dataset)
+
