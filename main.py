509d4024e56d7b89537be809bb685b249b904e27
StefanoColamonaco <stef_2000@hotmail.it>
2024-01-15 17:49:15 +0100

    Rerank implementation
    Co-authored-by: AndreaZecca <AndreaZecca@users.noreply.github.com>
    Co-authored-by: MatteoVannucchi0 <MatteoVannucchi0@users.noreply.github.com>
    Co-authored-by: elements72 <elements72@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_community.vectorstores import Milvus
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
+from FlagEmbedding import FlagReranker
+
 from pathlib import Path
 from dotenv import load_dotenv
 import json
 from tqdm import tqdm
 import click
+import numpy as np
 
 import warnings
 warnings.filterwarnings("ignore", message="")
 
+import transformers
+transformers.logging.set_verbosity_error()
 
 from utils import get_template, get_results_path
 from parse_dataset import parse_dataset, get_index_from_res
@@ -23,18 +28,33 @@ load_dotenv()
 templates_path = Path('./templates')
 
 usr_template = None
+RETRIEVE_WITH_RERANK = 10
+KEEP_TOP = 5
 
 
 global_context = []
+global_query = None
 
 embedder = HuggingFaceEmbeddings(model_name="neuml/pubmedbert-base-embeddings")
+reranker_model = FlagReranker('BAAI/bge-reranker-large', use_fp16=True) # use_fp16 speeds up computation with a slight performance degradation
 
 def format_docs(docs):
-    global global_context
-    global_context = [d.page_content for d in docs]
     return "\n\n".join([d.page_content for d in docs])
 
-def test_dataset(dataset, llm, model):
+def rerank_docs(docs):
+    global global_context
+    global global_query
+
+    # rerank docs
+    pairs = [list((global_query, d.page_content)) for d in docs]
+    scores = reranker_model.compute_score(pairs)
+    permutation = np.argsort(scores)[::-1]
+    docs = [docs[i] for i in permutation]
+    global_context = [d.page_content for d in docs]
+    docs = docs[:KEEP_TOP]
+    return docs
+
+def test_dataset(dataset, llm, model, rerank, debug):
     collection_name = 'medqa' if dataset in ['medqa_opt4', 'medqa_opt5'] else 'medmcqa'
 
     with open(get_template(model, dataset)) as f:
@@ -49,16 +69,24 @@ def test_dataset(dataset, llm, model):
     )
     
     # Retrieve and generate using the relevant snippets of the blog.
-    retriever = vector_db.as_retriever(search_kwargs={"k":5})
+    retriever = vector_db.as_retriever(search_kwargs={"k": RETRIEVE_WITH_RERANK if rerank else KEEP_TOP})
+
+    if rerank:
+        base_chain = {"context": retriever | rerank_docs | format_docs, "question": RunnablePassthrough()}
+    else:
+        base_chain = {"context": retriever | format_docs, "question": RunnablePassthrough()}
 
     chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        base_chain
         | prompt
         | llm
         | StrOutputParser()
     )
 
     questions = parse_dataset(f"{dataset}.jsonl")
+
+    if debug:
+        questions = questions[:3]
 
     correct = 0
     total_questions = len(questions)
@@ -67,6 +95,8 @@ def test_dataset(dataset, llm, model):
     
     for q in tqdm(questions):      
         question = q["question"]
+        global global_query
+        global_query = question
         response = chain.invoke(question)
         response_idx = get_index_from_res(response)
         answer_idx = q["answer_idx"]
@@ -86,14 +116,22 @@ def test_dataset(dataset, llm, model):
         "right_answers": correct,
         "accuracy": correct / total_questions
     }
-    with open(get_results_path(model, dataset), "w") as f:
-        json.dump(output_json, f)    
+    result_path = get_results_path(model, dataset)
+    if debug:
+        result_path = result_path.replace('.json', '_debug.json')
+    if rerank:
+        result_path = result_path.replace('.json', '_rerank.json')
+    with open(result_path, "w") as f:
+        json.dump(output_json, f, indent=4)   
+    return correct / total_questions 
 
 
 @click.command()
 @click.option("--model", help="Model to use", required=True)
 @click.option("--datasets", help="Dataset to use", required=True)
-def main(model, datasets):
+@click.option("--rerank", help="Whether to use reranking", required=False, default=False)
+@click.option("--debug", help="Debug mode", required=False, default=False)
+def main(model, datasets, rerank, debug):
     datasets = datasets.split(',')
     model_kwargs={"torch_dtype": "auto", "temperature": 1e-16, "do_sample": True}
     if 'mistral' in model:
@@ -107,7 +145,8 @@ def main(model, datasets):
     )
     for dataset in datasets:
         print(f'Testing model {model} with dataset {dataset}')
-        test_dataset(dataset, llm, model)
+        accuracy = test_dataset(dataset, llm, model, rerank, debug)
+        print(f'Accuracy: {accuracy:.2f}%')
         print()
 
 if __name__ == "__main__":
--- run_models.sh
+++ run_models.sh
@@ -1,7 +1,7 @@
 declare -a models=(
     'HuggingFaceH4/zephyr-7b-beta'
     'meta-llama/Llama-2-7b-chat-hf'
-    # 'mistralai/Mistral-7B-Instruct-v0.1'
+    'mistralai/Mistral-7B-Instruct-v0.1'
 )
 
 # declare a string variable 
@@ -11,13 +11,16 @@ declare -a datasets=(
     'medmcqa_opt4'
 )
 
+rerank=true
 
 datasets_string="${datasets[0]}"
+
+debug=false
 
 for dataset in "${datasets[@]:1}"; do
     datasets_string="$datasets_string,$dataset"
 done
 
 for model in "${models[@]}"; do
-    python3 main.py --model $model --datasets $datasets_string
+    python3 main.py --model $model --datasets $datasets_string --rerank $rerank --debug $debug
 done
