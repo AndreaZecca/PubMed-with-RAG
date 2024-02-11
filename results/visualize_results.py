112f91d96a41091b69bf62fcd0659742730077c1
StefanoColamonaco <stef_2000@hotmail.it>
2024-02-11 16:31:42 +0100

    Results in CVS
    Co-authored-by: AndreaZecca <AndreaZecca@users.noreply.github.com>

--- results/visualize_results.py
+++ results/visualize_results.py
@@ -2,14 +2,17 @@ import os
 import json
 import pandas as pd
 from tabulate import tabulate
+import re
 results = {}
+
+pd.options.display.float_format = '{:,.4f}'.format
 
 for model_name in os.listdir('.'):
     if not os.path.isdir(model_name) or model_name.startswith('.') or model_name.startswith('..') or model_name.startswith('other_results'):
         continue
     results[model_name] = {}
     for dataset_name in sorted(os.listdir(f'{model_name}/')):
-        if model_name.startswith('.') or model_name.startswith('..') or model_name.endswith('.py') or 'debug' in dataset_name or 'textbooks' in dataset_name or 'artificial' not in dataset_name:
+        if model_name.startswith('.') or model_name.startswith('..') or model_name.endswith('.py') or 'debug' in dataset_name or 'textbooks' in dataset_name:
             continue
         with open(f'{model_name}/{dataset_name}', 'r') as f:
             data = json.load(f)
@@ -21,5 +24,6 @@ df = pd.DataFrame(results).transpose()
 df = df.sort_index()
 # sort columns by name
 df = df[df.columns.sort_values()]
-# df.to_csv('all_results.csv')
-print(tabulate(df, headers='keys', tablefmt='psql'))
+print(tabulate(df.T, headers='keys', tablefmt='psql'))
+
+df.T.to_csv('results.csv', float_format='%.4f') 
